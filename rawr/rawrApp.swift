ab55356d18ea866c7ab5c95c4985ef0c54d670e6
Michael Teuscher <michael.teuscher@pm.me>
2023-08-12 14:42:40 +0200

    Initial Commit

---
+++ rawr/rawrApp.swift
@@ -0,0 +1,17 @@
+//
+//  rawrApp.swift
+//  rawr
+//
+//  Created by Nila on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct rawrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
