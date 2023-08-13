b1f230ea23b61e421a8ccde354f463944bf9d31f
Michael Teuscher <michael.teuscher@pm.me>
2023-08-13 16:30:43 +0200

    re-align timeline header, move to component

--- rawr/UI/Timeline/TimelineView.swift
+++ rawr/UI/Timeline/TimelineView.swift
@@ -23,56 +23,7 @@ struct TimelineView: View {
             if selectedTab == 2 {
                 Timeline(timelineContext: TimelineContext(timelineType: .GLOBAL)).padding(.top, 55)
             }
-
-            ZStack {
-                HStack(alignment: .center) {
-                    ProfileSwitcher(context: self.context)
-                        .frame(width: 40, height: 40)
-                        .padding(.trailing, 10)
-                        .padding(.top, 5)
-                        .padding(.bottom, -5)
-                    Spacer()
-                    Image(systemName: "antenna.radiowaves.left.and.right")
-                        .font(.system(size: 20))
-                        .padding(.leading, 10)
-                        .padding(.top, 5)
-                }
-                Menu {
-                    Button("Home") {
-                        selectedTab = 0
-                    }
-                    Button("Local") {
-                        selectedTab = 1
-                    }
-                    Button("Global") {
-                        selectedTab = 2
-                    }
-                } label: {
-                    VStack {
-                        Text(self.context.currentInstanceName).font(.system(size: 20, weight: .semibold)).foregroundColor(.primary)
-                        HStack {
-                            Group {
-                                if (selectedTab == 0) {
-                                    Text("Home")
-                                }
-                                if (selectedTab == 1) {
-                                    Text("Local")
-                                }
-                                if (selectedTab == 2) {
-                                    Text("Global")
-                                }
-                            }.foregroundColor(.primary.opacity(0.7))
-                                .font(.system(size: 16))
-                                .padding(.top, -11)
-                                .padding(.bottom, -10)
-                            Image(systemName: "chevron.up.chevron.down")
-                                .font(.system(size: 10))
-                                .foregroundColor(.primary.opacity(0.7))
-                                .padding(.leading, -5)
-                        }.padding(.leading, 10).padding(.top, -5).padding(.bottom, -5)
-                    }
-                }
-            }.padding(.horizontal).padding(.bottom).background(.thinMaterial)
+            TimelineHeader(context: self.context, selectedTab: self.$selectedTab)
         }
     }
 }
