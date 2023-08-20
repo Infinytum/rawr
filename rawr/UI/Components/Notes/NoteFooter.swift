8a485c1275d0824ee718bfa4df78b0ce0e9957e7
Dråfølin <66629792+drafolin@users.noreply.github.com>
2023-08-20 16:11:50 +0200

    emoji picker (#8)
    * reaction implemented.
    
    * code cleanup
    
    * removed broken animation

--- rawr/UI/Components/Notes/NoteFooter.swift
+++ rawr/UI/Components/Notes/NoteFooter.swift
@@ -7,18 +7,40 @@
 
 import SwiftUI
 import MisskeyKit
+import WrappingHStack
+
+struct emoji {
+    var name: String
+    var image: RemoteImage?
+    var emojiChar: Text?
+}
 
 struct NoteFooter: View {
     @EnvironmentObject var context: ViewContext
 
     @ObservedObject var note: NoteModel
     @State var didRenote: Bool = false
+    @State var emojiPickerShown = false
+    @State private var emojiList: [String: [emoji]] = [:]
+    @State private var favouriteEmojis: [emoji] = []
+    
+    private var emojiCategoriesArray: [String]{
+        var initial = Array(emojiList.keys).sorted(by:<)
+        if initial.count == 0 {
+            return []
+        }
+        initial.removeLast()
+        initial.insert("uncategorized", at: 0)
+        return initial
+    }
     
     var body: some View {
         HStack() {
-            HStack {
-                Image(systemName: "text.bubble").fontWeight(.light).padding(.bottom, -2)
-                Text(String(self.note.repliesCount ?? 0)).font(.system(size: 16, weight: .light))
+            Button {} label: {
+                HStack {
+                    Image(systemName: "text.bubble").fontWeight(.light).padding(.bottom, -2)
+                    Text(String(self.note.repliesCount ?? 0)).font(.system(size: 16, weight: .light))
+                }
             }
             Spacer()
             Menu {
@@ -39,32 +61,99 @@ struct NoteFooter: View {
                 }
             }.foregroundStyle(.primary)
             Spacer()
-            HStack {
-                Image(systemName: "star").foregroundColor(self.note.myReaction != nil ? .yellow : .primary).fontWeight(self.note.myReaction != nil ? .bold : .light)
-                Text(String(self.note.reactionsCount())).font(.system(size: 16, weight: .light))
-            }.onTapGesture {
+            Button {
                 self.onReact()
+            } label: {
+                    Image(systemName: self.note.myReaction != nil ? "minus" : "star")
+                        .foregroundColor(.primary)
+                        .fontWeight(self.note.myReaction != nil ? .bold : .light)
             }
             Spacer()
-            Image(systemName: "hands.and.sparkles")
-                .fontWeight(.light)
+            Button {
+                emojiPickerShown = true
+            } label: {
+                HStack {
+                    Image(systemName: "hands.sparkles")
+                        .fontWeight(.light)
+                    Text(String(self.note.reactionsCount())).font(.system(size: 16, weight: .light))
+                }
+            }
+            .sheet(isPresented: $emojiPickerShown) {
+                List {
+                    ForEach(emojiCategoriesArray, id: \.self) {category in
+                        EmojiSection(emojiContent: emojiList[category]!, categoryName: category.replacingOccurrences(of: "_", with: " ")) {name in
+                            if emojiList[category]?.first(where: {$0.name == name})?.name == nil {
+                                print(category)
+                            }
+                            
+                            onReact(emojiList[category]!.first{$0.name == name}!.name)
+                        }
+                    }
+                }
+                .onAppear {
+                    fillEmojiList()
+                }
+                .listSectionSeparator(.hidden)
+                .listRowSeparator(.hidden)
+                .listRowSpacing(.zero)
+            }
             Spacer()
-            Image(systemName: "quote.bubble")
-                .fontWeight(.light)
+            Button {} label: {
+                Image(systemName: "quote.bubble")
+                    .fontWeight(.light)
+            }
+        }
+        .foregroundStyle(.primary)
+    }
+    
+    private func fillEmojiList() {
+        emojiList = [:]
+        if context != nil {
+            for emoji in context.currentInstance?.emojis ?? [] {
+                let category = emoji.category ?? "uncategorized"
+                var tempArray = emojiList[category] ?? []
+                tempArray.append(
+                    .init(name: ":\(emoji.name!):", image: RemoteImage(emoji.url))
+                )
+                emojiList[category] = tempArray
+            }
+        }
+        
+        MisskeyKit.shared.emojis.getDefault {emojis in
+            guard let emojis = emojis else {
+                return
+            }
+            emojis.forEach {emoji in
+                let category = emoji.category
+                var tempArray = emojiList[category?.rawValue ?? "uncategorized"] ?? []
+                
+                guard emoji.char != nil else {
+                    return
+                }
+                
+                tempArray.append(
+                    .init(name: ":\(emoji.name!):", emojiChar: Text(emoji.char!))
+                )
+                emojiList[category?.rawValue ?? "uncategorized"] = tempArray
+            }
         }
     }
     
-    private func onReact() {
-        let reaction = self.context.currentInstance?.defaultReaction ?? ":star:"
+    private func onReact(_ r: String? = nil) {
+        let reaction: String = r ?? context.currentInstance?.defaultReaction ?? ":star:"
         Task {
             do {
-                if note.isMyReaction(reaction) {
+                if note.isMyReaction(reaction) ||
+                    (r == nil && note.myReaction != nil) {
+                    print("unreacting.")
                     try await note.unreact()
                 } else {
+                    print("is not my reaction, reacting with \(reaction)")
                     try await note.react(reaction)
                 }
-            } catch {
-                // TODO: Show error toasts on error
+                emojiPickerShown = false
+            } catch let err {
+                print(err.localizedDescription)
             }
         }
     }
