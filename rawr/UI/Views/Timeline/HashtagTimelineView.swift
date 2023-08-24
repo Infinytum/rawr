24c0500794de391d95c0c67b3702dbea8ba35e79
Michael Teuscher <michael.teuscher@pm.me>
2023-08-24 13:12:49 +0200

    Improve Hashtag header, re-order stuff

---
+++ rawr/UI/Views/Timeline/HashtagTimelineView.swift
@@ -0,0 +1,18 @@
+//
+//  HashtagTimelineView.swift
+//  rawr.
+//
+//  Created by Nila on 24.08.2023.
+//
+
+import SwiftUI
+
+struct HashtagTimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HashtagTimelineView()
+}
