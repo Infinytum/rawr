7c6a1f99a3528407311b41af2ca5c04789fac24e
Michael Teuscher <michael.teuscher@pm.me>
2023-10-12 23:36:21 +0200

    Also switch note decoration to NavStack

--- rawr/UI/Components/Notes/Decorations/NoteDecorationBoost.swift
+++ rawr/UI/Components/Notes/Decorations/NoteDecorationBoost.swift
@@ -10,33 +10,25 @@ import MisskeyKit
 
 struct NoteDecorationBoost: View {
     @ObservedObject var note: NoteModel
-    @ObservedObject var viewRefresher = ViewReloader()
-    
-    @State var presentUserSheet = false
-    @State var presentedUser = ""
 
     var body: some View {
-        HStack {
-            Image(systemName: "arrow.2.squarepath")
-                .padding(.leading, 12)
-            Group {
-                Text("Boosted by ")
-                    .foregroundColor(.primary.opacity(0.7)) +
-                Text(self.note.user?.name ?? "<no name>")
-                    .foregroundColor(.blue)
-            }.font(.system(size: 15))
-                .padding(.leading, 12)
-            Spacer()
-            Text(self.note.relativeCreatedAtTime())
-                .foregroundColor(.primary.opacity(0.7))
-                .font(.system(size: 15))
-        }.sheet(isPresented: $presentUserSheet) {
-            UserView(userName: presentedUser)
-        }.onTapGesture {
-            self.presentedUser = "\(self.note.user?.username ?? "")@\(self.note.user?.host ?? "")"
-            self.viewRefresher.reloadView()
-            self.presentUserSheet = true
-        }
+        NavigationLink(value: TappedMention(username: "\(self.note.user!.username!)@\(self.note.user!.host!)")) {
+            HStack {
+                Image(systemName: "arrow.2.squarepath")
+                    .padding(.leading, 12)
+                Group {
+                    Text("Boosted by ")
+                        .foregroundColor(.primary.opacity(0.7)) +
+                    Text(self.note.user?.name ?? "<no name>")
+                        .foregroundColor(.blue)
+                }.font(.system(size: 15))
+                    .padding(.leading, 12)
+                Spacer()
+                Text(self.note.relativeCreatedAtTime())
+                    .foregroundColor(.primary.opacity(0.7))
+                    .font(.system(size: 15))
+            }
+        }.buttonStyle(PlainButtonStyle())
     }
 }
 
