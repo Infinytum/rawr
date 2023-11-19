8169f511e8693d11d181cd3618cf76d8f50835f1
Michael Teuscher <michael.teuscher@pm.me>
2023-11-19 15:11:16 +0100

    Improve Mention parsing

--- rawr/Modules/MFM/Tokenizer/TokenizerTests.swift
+++ rawr/Modules/MFM/Tokenizer/TokenizerTests.swift
@@ -1,45 +1,45 @@
 //
-//  NoteTokenizer.swift
+//  TokenizerTests.swift
 //  rawr.
 //
 //  Created by Nila on 15.11.2023.
 //
 
 import Foundation
-import MisskeyKit
 import SwiftUI
 
 #Preview {
     ScrollView([.horizontal, .vertical]) {
         Visualizer(rootNode: Tokenizer.note.tokenize(
 """
-Hello World
-
-This is a test note to test tokenizing capabilities of rawr.
-
-As you can see we need to test every possible module :star:
-
-<small>Well you are about to see at least</small>
-
-This test note was written by @nila also known as @nila@derg.social
-- Pretty cool right?
-
-<center>So why did I write my name twice?
-So I could check for username only and full mentions!</center>
-
-**Not because I like to see my name**
-
-<small><i>Getting awkward...</i></small>
-
-$[x2 All in the name of a complete test post!]
-
-Check out my website https://bad-dragon.ch
-
-Or my [GitHub](https://github.com/nilathedragon)
-
-#dragon #rawr #app
+Plain Text#hashtags@username@instance.tld$[x2 Double Size Modifier][Named URL](https://example.com)**A bold claim**<center><small><i>Italic, small and centered</i></small></center>https://example.com
 """
         )).scaleEffect(0.5)
+    }
+    .defaultScrollAnchor(.center)
+    .previewDisplayName("Test Note 1")
+}
+
+#Preview {
+    ScrollView([.horizontal, .vertical]) {
+        Visualizer(rootNode: Tokenizer.note.tokenize(
+"""
+Hello World
+This is a test note to test tokenizing capabilities of rawr.
+As you can see we need to test every possible module :star:
+<small>Well you are about to see at least</small>
+This test note was written by @nila also known as @nila@derg.social
+- Pretty cool right?
+<center>So why did I write my name twice?
+So I could check for username only and full mentions!</center>
+**Not because I like to see my name**
+<small><i>Getting awkward...</i></small>
+$[x2 All in the name of a complete test post!]
+Check out my website https://bad-dragon.ch
+Or my [GitHub](https://github.com/nilathedragon)
+#dragon #rawr #app
+"""
+        )).scaleEffect(0.4)
     }
     .defaultScrollAnchor(.center)
     .previewDisplayName("Test Note 1")
